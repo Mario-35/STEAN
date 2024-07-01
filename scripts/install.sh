5c2a47228c9276fd250a9dd13a2158ca353cd75f
Adam <mario.adam@inrae.fr>
2024-07-01 20:31:41 +0200

    script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,29 +3,53 @@
  #
  # @copyright 2024-present Inrae
  # @author mario.adam@inrae.fr
- # version 0.5
+ # version 0.9
  #
  #/
 
 clear
-APIDEST=api
-APIBak=apiBak
-FILEAPP=./$APIDEST/index.js
+# Check if the script is run as root
+# if [ "$EUID" -ne 0 ]; then
+#   echo "Please run this script as root or use sudo"
+#   exit 1
+# fi
+
+# Prompt for the domain name and directory
+read -p "Enter the path to install api (/var/www/stean): " APIDEST
 FILEDIST=./dist.zip
 FILEDISTOLD=./distOld.zip
-FILEKEY=./$APIDEST/configuration/.key
-FILECONFIG=./$APIDEST/configuration/configuration.json
-HELLOSCRIPT=./hello.sql
+FILERUN=./run.sh
+
+create_run() {
+    if [ -f $FILERUN ];
+    then
+        rm $FILERUN
+        echo "Delete => $FILERUN"
+    fi
+    echo "#!/bin/bash" > $FILERUN
+    echo "pm2 stop index" >> $FILERUN
+    echo "pm2 flush" >> $FILERUN
+    echo "pm2 delete index" >> $FILERUN
+    echo "echo \"API starting ...\"" >> $FILERUN
+    echo "NODE_ENV=production" >> $FILERUN
+    echo "mv $APIDEST/api/logs.html $APIDEST/logs.bak" >> $FILERUN
+    echo "pm2 start $APIDEST/api/index.js" >> $FILERUN
+    echo "pm2 logs --lines 500" >> $FILERUN
+    sudo chmod -R 777 $FILERUN
+    echo "Create script => $FILERUN"
+}
 
 # Script to install Node.js using Node on Ubuntu without sudo
-
 logo() {
+    echo ""
     echo "  ____ __________    _     _   _ "
     echo " / ___|_ __  ____|  / \   | \ | |"
     echo " \___ \| | |  _|   / _ \  |  \| |"
     echo "  ___) | | | |___ / ___ \ | |\  |"
     echo " |____/|_| |_____|_/   \_\|_| \_|"
+    echo ""
 }
+
 # Function to install Node
 install_node() {
     echo "Installing Node..."
@@ -35,8 +59,7 @@ install_node() {
 # Function to install postgresql-postgis
 install_pg() {
     echo "Installing postgresql-postgis ..."
-    sudo apt autoremove
-    sudo apt install postgis postgresql-14-postgis-3 -y -qq
+    sudo apt install postgis postgresql-14-postgis-3 -y
 }
 
 # Function to install pm2
@@ -51,54 +74,57 @@ install_unzip() {
     sudo apt-get install unzip
 }
 
+# Function to make bak 
 save_dist() {
     if [ -f "$FILEDIST" ]; then
-        rm $FILEDISTOLD
+        rm -f $FILEDISTOLD
+        echo "Delete => $FILEDISTOLD"
         mv $FILEDIST $FILEDISTOLD
+        echo "Move $FILEDIST => $FILEDISTOLD"
     fi
 }
 
 # Function to get stean
 download_stean() {
-    echo "Downloading stean..."
     save_dist
     sudo curl -o $FILEDIST -L https://github.com/Mario-35/STEAN/raw/main/dist.zip
-    sudo curl -o $HELLOSCRIPT -L https://github.com/Mario-35/STEAN/raw/main/scripts/hello.sql
-}
-
-# Function to create run.sh
-create_run() {
-    echo "Create run.sh"
-    cp ./$APIDEST/scripts/run.sh .
-    sudo chmod -R 777 run.sh
-    cp ./$APIDEST/scripts/back.sh .
-    sudo chmod -R 777 back.sh
+    echo "Downloading => $FILEDIST"
 }
 
 # Function to install stean
 install_stean() {
     stop_stean
     # remove bak
-    rm -r $APIBak
+    if [ -f $APIDEST/apiBak ];
+    then
+        rm -r $APIDEST/apiBak
+        echo "Delete => $APIDEST/apiBak"
+    fi
     # save actual to bak
-    mv $APIDEST $APIBak
+    if [ -f $APIDEST/api ];
+    then
+        mv $APIDEST/api $APIDEST/apiBak
+        echo "Move $APIDEST/api => $APIDEST/apiBak"
+    fi
+    # create path
+    sudo mkdir -p -m 777 $APIDEST/api
+    echo "Create folder => $APIDEST/api"
     # unzip actual
-    unzip -qq $FILEDIST -d $APIDEST/  
+    unzip -qq $FILEDIST -d $APIDEST/api/  
+    echo "unzip $FILEDIST => $APIDEST/api"
     # Save config
-    if [ -f ./$APIBak/configuration/configuration.json ]; then
-        echo "confifuration exist."
-        cp ./$APIBak/configuration/configuration.json ./$APIDEST/configuration/configuration.json
+    if [ -f $APIDEST/apiBak/configuration/configuration.json ]; then
+        cp $APIDEST/apiBak/configuration/configuration.json $APIDEST/api/configuration/configuration.json
+        echo "Move $APIDEST/apiBak/configuration/configuration.json => $APIDEST/api/configuration/configuration.json"
     fi
     # Save key
-    if [ -f ./$APIBak/configuration/.key ]; then
-        echo "Key exists."
-        cp ./$APIBak/configuration/.key ./$APIDEST/configuration/.key
+    if [ -f $APIDEST/apiBak/configuration/.key ]; then
+        cp $APIDEST/apiBak/configuration/.key $APIDEST/api/configuration/.key
+        echo "Move $APIDEST/apiBak/configuration/.key => $APIDEST/api/configuration/.key"        
     fi
     save_dist
-    create_run
-    cd $APIDEST
-    npm install --silent --omit=dev
-    cd ..
+    cd $APIDEST/api
+    npm install --silent --omit=dev --prefix $APIDEST/api/
 }
 
 stop_stean() {
@@ -110,13 +136,12 @@ stop_stean() {
 #|                        START                                   |
 #------------------------------------------------------------------
 echo "---------- Installation ---------"
-logo
 # Check if PostgreSQL  is installed
-if ! command -v psql --version &> /dev/null
+if ! command -v psql --version > /dev/null
 then
     echo "PostgreSQL is Not installed."
     install_pg
-    if ! command -v psql --version &> /dev/null
+    if ! command -v psql --version > /dev/null
     then
         exit
     fi
@@ -124,9 +149,8 @@ else
     echo "PostgreSQL is installed."
 fi
 
-
 # Check if Node is installed
-if ! command -v node &> /dev/null
+if ! command -v node > /dev/null
 then
     install_node
 else
@@ -134,7 +158,7 @@ else
 fi
 
 # Check if pm2 is installed
-if ! command -v pm2 &> /dev/null
+if ! command -v pm2 > /dev/null
 then
     install_pm2
 else
@@ -142,13 +166,12 @@ else
 fi
 
 # Check if unzip is installed
-if ! command -v unzip &> /dev/null
+if ! command -v unzip > /dev/null
 then
     install_unzip
 else
     echo "unzip is already installed."
 fi
-
 
 if [ -f $FILEDIST ];
 then
@@ -165,10 +188,6 @@ else
     download_stean
 fi
 
-psql -U postgres -f $HELLOSCRIPT
-
 install_stean
-sh ./run.sh
-install_stean
-echo "------------ Installed ----------"
+create_run
 logo
