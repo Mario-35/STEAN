e424a76c3027425e1cbd3765929a7980e7928c73
Adam <40055158+Mario-35@users.noreply.github.com>
2024-07-02 07:36:46 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
-file_dist_old #/
+ #/
  # Stean Install Bash.
  #
  # @copyright 2024-present Inrae
@@ -8,6 +8,7 @@ file_dist_old #/
  #/
 
 clear
+
 # Check if the script is run as root
 # if [ "$EUID" -ne 0 ]; then
 #   echo "Please run this script as root or use sudo"
@@ -15,31 +16,34 @@ clear
 # fi
 
 # Prompt for the domain name and directory
-read -p "Enter the path to install api (/var/www/stean): " api_dest
-file_dist=./dist.zip
-file_dist_old=./distOld.zip
-script_run=./run.sh
+read -p "Enter the path to install api (/var/www/stean): " APIDEST
+# Name of the file downladed
+FILEDIST=./dist.zip
+# Name of the backup
+FILEDISTOLD=./distOld.zip
+# Name of the run script
+FILERUN=./run.sh
 
+# Create run script
 create_run() {
-    if [ -f $script_run ];
-    then
-        rm $script_run
-        echo "Delete => $script_run"
+    if [ -f $FILERUN ]; then
+        rm $FILERUN
+        echo "Delete => $FILERUN"
     fi
-    echo "#!/bin/bash" > $script_run
-    echo "pm2 stop index" >> $script_run
-    echo "pm2 flush" >> $script_run
-    echo "pm2 delete index" >> $script_run
-    echo "echo \"API starting ...\"" >> $script_run
-    echo "NODE_ENV=production" >> $script_run
-    echo "mv $api_dest/api/logs.html $api_dest/logs.bak" >> $script_run
-    echo "pm2 start $api_dest/api/index.js" >> $script_run
-    echo "pm2 logs --lines 500" >> $script_run
-    sudo chmod -R 777 $script_run
-    echo "Create script => $script_run"
+    echo "#!/bin/bash" > $FILERUN
+    echo "pm2 stop index" >> $FILERUN
+    echo "pm2 flush" >> $FILERUN
+    echo "pm2 delete index" >> $FILERUN
+    echo "echo \"API starting ...\"" >> $FILERUN
+    echo "NODE_ENV=production" >> $FILERUN
+    echo "mv $APIDEST/api/logs.html $APIDEST/logs.bak" >> $FILERUN
+    echo "pm2 start $APIDEST/api/index.js" >> $FILERUN
+    echo "pm2 logs --lines 500" >> $FILERUN
+    sudo chmod -R 777 $FILERUN
+    echo "Create script => $FILERUN"
 }
 
-# Script to install Node.js using Node on Ubuntu without sudo
+# Function to show logo
 logo() {
     echo ""
     echo "  ____ __________    _     _   _ "
@@ -50,143 +54,138 @@ logo() {
     echo ""
 }
 
-# Function to install Node
-install_node() {
-    echo "Installing Node..."
-    sudo apt install nodejs
+# Function to check Node and install it if not
+check_node() {
+    if ! command -v node > /dev/null
+    then
+        echo "Installing Node..."
+        sudo apt install nodejs
+    else
+        echo "Node is already installed."
+    fi    
 }
 
-# Function to install postgresql-postgis
-install_pg() {
-    echo "Installing postgresql-postgis ..."
-    sudo apt install postgis postgresql-14-postgis-3 -y
+# Function to check PostgreSQL-postgis and install it if not
+check_pg() {
+    if ! command -v psql --version > /dev/null
+    then
+        echo "Installing postgresql-postgis ..."
+        sudo apt install postgis postgresql-14-postgis-3 -y
+        if ! command -v psql --version > /dev/null
+        then
+            exit
+        fi
+    else
+        echo "PostgreSQL is already installed."
+    fi    
 }
 
-# Function to install pm2
-install_pm2() {
-    echo "Installing pm2..."
-    sudo npm install pm2@latest -g
+# Function to check pm2 and install it if not
+check_pm2() {
+    if ! command -v pm2 > /dev/null
+    then
+        echo "Installing pm2..."
+        sudo npm install pm2@latest -g
+    else
+        echo "pm2 is already installed."
+    fi    
 }
 
-# Function to install unzip
-install_unzip() {
-    echo "Installing unzip..."
-    sudo apt-get install unzip
+# Function to check unzip and install it if not
+check_unzip() {
+    if ! command -v unzip > /dev/null
+    then
+        echo "Installing unzip..."
+        sudo apt-get install unzip
+    else
+        echo "unzip is already installed."
+    fi
+}
+
+# Function to check dist file
+check_dist() {
+    # Check if file already present and ask to use it if true
+    if [ -f $FILEDIST ];
+    then
+        echo "$FILEDIST is already present."
+        while true; do
+            read -p "Do you wish to use it " yn
+            case $yn in
+                [Yy]* ) break;;
+                [Nn]* ) download_dist; break;;
+                * ) echo "Please answer yes or no.";;
+            esac
+        done
+    else
+        download_dist
+    fi
 }
 
 # Function to make bak 
 save_dist() {
-    if [ -f "$file_dist" ]; then
-        rm -f $file_dist_old
-        echo "Delete => $file_dist_old"
-        mv $file_dist $file_dist_old
-        echo "Move $file_dist => $file_dist_old"
+    if [ -f "$FILEDIST" ]; then
+        rm -f $FILEDISTOLD
+        echo "Delete => $FILEDISTOLD"
+        mv $FILEDIST $FILEDISTOLD
+        echo "Move $FILEDIST => $FILEDISTOLD"
     fi
 }
 
 # Function to get stean
-download_stean() {
+download_dist() {
     save_dist
-    sudo curl -o $file_dist -L https://github.com/Mario-35/STEAN/raw/main/dist.zip
-    echo "Downloading => $file_dist"
+    sudo curl -o $FILEDIST -L https://github.com/Mario-35/STEAN/raw/main/dist.zip
+    echo "Downloading => $FILEDIST"
 }
 
 # Function to install stean
 install_stean() {
-    stop_stean
     # remove bak
-    if [ -f $api_dest/apiBak ];
-    then
-        rm -r $api_dest/apiBak
-        echo "Delete => $api_dest/apiBak"
+    if [ -f $APIDEST/apiBak ]; then
+        rm -r $APIDEST/apiBak
+        echo "Delete => $APIDEST/apiBak"
     fi
     # save actual to bak
-    if [ -f $api_dest/api ];
-    then
-        mv $api_dest/api $api_dest/apiBak
-        echo "Move $api_dest/api => $api_dest/apiBak"
+    if [ -f $APIDEST/api ]; then
+        mv $APIDEST/api $APIDEST/apiBak
+        echo "Move $APIDEST/api => $APIDEST/apiBak"
     fi
     # create path
-    sudo mkdir -p -m 777 $api_dest/api
-    echo "Create folder => $api_dest/api"
+    sudo mkdir -p -m 777 $APIDEST/api
+    echo "Create folder => $APIDEST/api"
     # unzip actual
-    unzip -qq $file_dist -d $api_dest/api/  
-    echo "unzip $file_dist => $api_dest/api"
+    unzip -qq $FILEDIST -d $APIDEST/api/  
+    echo "unzip $FILEDIST => $APIDEST/api"
     # Save config
-    if [ -f $api_dest/apiBak/configuration/configuration.json ]; then
-        cp $api_dest/apiBak/configuration/configuration.json $api_dest/api/configuration/configuration.json
-        echo "Move $api_dest/apiBak/configuration/configuration.json => $api_dest/api/configuration/configuration.json"
+    if [ -f $APIDEST/apiBak/configuration/configuration.json ]; then
+        cp $APIDEST/apiBak/configuration/configuration.json $APIDEST/api/configuration/configuration.json
+        echo "Move $APIDEST/apiBak/configuration/configuration.json => $APIDEST/api/configuration/configuration.json"
     fi
     # Save key
-    if [ -f $api_dest/apiBak/configuration/.key ]; then
-        cp $api_dest/apiBak/configuration/.key $api_dest/api/configuration/.key
-        echo "Move $api_dest/apiBak/configuration/.key => $api_dest/api/configuration/.key"        
+    if [ -f $APIDEST/apiBak/configuration/.key ]; then
+        cp $APIDEST/apiBak/configuration/.key $APIDEST/api/configuration/.key
+        echo "Move $APIDEST/apiBak/configuration/.key => $APIDEST/api/configuration/.key"        
     fi
     save_dist
-    npm install --silent --omit=dev --prefix $api_dest/api/
+    cd $APIDEST/api
+    npm install --silent --omit=dev --prefix $APIDEST/api/
 }
 
+# Function to stop stean
 stop_stean() {
     pm2 stop index
     pm2 kill
 }
 
-#------------------------------------------------------------------
-#|                        START                                   |
-#------------------------------------------------------------------
-echo "---------- Installation ---------"
-# Check if PostgreSQL  is installed
-if ! command -v psql --version > /dev/null
-then
-    echo "PostgreSQL is Not installed."
-    install_pg
-    if ! command -v psql --version > /dev/null
-    then
-        exit
-    fi
-else
-    echo "PostgreSQL is installed."
-fi
-
-# Check if Node is installed
-if ! command -v node > /dev/null
-then
-    install_node
-else
-    echo "Node is already installed."
-fi
-
-# Check if pm2 is installed
-if ! command -v pm2 > /dev/null
-then
-    install_pm2
-else
-    echo "pm2 is already installed."
-fi
-
-# Check if unzip is installed
-if ! command -v unzip > /dev/null
-then
-    install_unzip
-else
-    echo "unzip is already installed."
-fi
-
-if [ -f $file_dist ];
-then
-    echo "$file_dist is already present."
-    while true; do
-        read -p "Do you wish to use it " yn
-        case $yn in
-            [Yy]* ) break;;
-            [Nn]* ) download_stean; break;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
-else
-    download_stean
-fi
-
+echo "------------------------------------------------------------------"
+echo "|                         STEAN Install                          |"
+echo "------------------------------------------------------------------"
+check_pg
+check_node
+check_pm2
+check_unzip
+check_dist
+stop_stean
 install_stean
 create_run
 logo
